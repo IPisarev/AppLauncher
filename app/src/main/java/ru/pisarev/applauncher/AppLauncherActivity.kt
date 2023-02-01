b0c35f6a77bd7a581cfa2bd32313a88e95aa33fd
pisarev <ivpisarev@yandex.ru>
2023-02-01 15:00:43 +0300

    Initial commit

---
+++ app/src/main/java/ru/pisarev/applauncher/AppLauncherActivity.kt
@@ -0,0 +1,11 @@
+package ru.pisarev.applauncher
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AppLauncherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_app_launcher)
+    }
+}
